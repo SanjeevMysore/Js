926ba8de6b859bd6465bb89ebf2a7bbdec24e312
SanjeevMysore <81816168+SanjeevMysore@users.noreply.github.com>
2022-09-27 00:21:15 +0530

    Create sentence.js

---
+++ sentence.js
@@ -0,0 +1,6 @@
+function myfun(noun,verb,adjective,adverb){
+  var result="";
+  result+="My "+ adjective + noun + verb + adverb + "towards me"
+  return result;
+}
+console.log(myfun(" dog"," ran","cute"," quickly "))
