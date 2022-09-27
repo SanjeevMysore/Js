21328143674a328244dcb0ade03f7b45201a08fb
SanjeevMysore <81816168+SanjeevMysore@users.noreply.github.com>
2022-09-27 20:05:22 +0530

    Create arrayoperations.js

---
+++ arrayoperations.js
@@ -0,0 +1,11 @@
+var myarray=[["hello","mate"],["how"]]
+myarray.push(["are"],["you"])
+console.log(myarray)
+var pop=myarray.pop()
+console.log(pop)
+var shift=myarray.shift()
+console.log(shift)
+var myarray2=[["Hello"],["can I knaow Your Name"]]
+myarray2.shift()
+myarray2.unshift(["I am Rick"])
+console.log(myarray2)
